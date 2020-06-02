4ee8be4e7705c7d78b00c05bae462be99f30c37d
Kyle Stewart <kylestew@gmail.com>
2020-06-01 22:11:58 -0700

    ...

--- build/tests/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kylestewart/Projects/algo-study/tests/googletest
-# Build directory: /Users/kylestewart/Projects/algo-study/build/tests/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/tests/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kylestewart/Projects/algo-study/tests/googletest/googletest
-# Build directory: /Users/kylestewart/Projects/algo-study/build/tests/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
