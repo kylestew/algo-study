e24f4261f07afba3df1c09d4502976dd3f86b00f
Kyle Stewart <kylestew@gmail.com>
2020-06-10 12:11:42 -0700

    Cool stack based binary conversion trick

--- cpp/tests/StackTests.cpp
+++ cpp/tests/StackTests.cpp
@@ -1,13 +1,37 @@
-t decNumber) { return ""; }
+#include <gtest/gtest.h>
 
-int main() { BST bst; }
+#include <stack>
+#include <string>
 
-// TEST(StackTests, ConvertSimple) {
-//     int given = 42;
-//     string expected = "101010";
-//     string result = divideBy2(given);
-//     EXPECT_EQ(result, expected);
-// }
+using namespace std;
+
+string divideBy2(int decNumber) {
+    stack<int> remstack;
+
+    while (decNumber > 0) {
+        // if /2 has a remainder, a 1 exist at the current LSB
+        int rem = decNumber % 2;
+        remstack.push(rem);
+        decNumber /= 2;
+    }
+
+    // stack now contains binary version of decimal number
+    // convert to string
+    string binString = "";
+    while (!remstack.empty()) {
+        binString.append(to_string(remstack.top()));
+        remstack.pop();
+    }
+
+    return binString;
+}
+
+TEST(StackTests, ConvertSimple) {
+    int given = 42;
+    string expected = "101010";
+    string result = divideBy2(given);
+    EXPECT_EQ(result, expected);
+}
 
 /*
 TEST(StackTests, InsertValue) {
