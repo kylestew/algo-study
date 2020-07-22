8289306e2fefd5722ffabf89725d292e28c34538
Kyle Stewart <kylestew@gmail.com>
2020-07-22 10:00:02 -0700

    Xcode project for swift study

---
+++ swift/AlgoStudy/AlgoStudy.h
@@ -0,0 +1,19 @@
+//
+//  AlgoStudy.h
+//  AlgoStudy
+//
+//  Created by Kyle Stewart on 7/22/20.
+//  Copyright © 2020 Kyle Stewart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlgoStudy.
+FOUNDATION_EXPORT double AlgoStudyVersionNumber;
+
+//! Project version string for AlgoStudy.
+FOUNDATION_EXPORT const unsigned char AlgoStudyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlgoStudy/PublicHeader.h>
+
+
